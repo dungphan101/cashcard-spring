e26fc69b827cde99e972d296cf5ccd685433f235
dungptm <ptmydung101097@gmail.com>
2025-05-19 11:56:08 +0700

    deleted files

--- src/main/resources/db/migration/202505090001__insert_data_to_bookmarks_dml.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO bookmarks (id, title, url, created_at, category_id, status)
-VALUES
-    (10, 'Byte byte go', 'https://bytebytego.com/', NOW(), 1, 'ACTIVE');
--- src/main/resources/db/migration/202505120001__rename_cfk_bookmarks_on_category_ddl.sql
+++
@@ -1,5 +0,0 @@
-# ALTER TABLE bookmarks
-#     DROP FOREIGN KEY FK_BOOKMARKS_ON_CATEGORY;
-#
-# ALTER TABLE bookmarks
-#     ADD CONSTRAINT fk_bookmarks_on_category FOREIGN KEY (category_id) REFERENCES categories (id);
