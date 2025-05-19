48a9f3b14c7abbf42a877920dc752a56787bd5d0
dungptm <ptmydung101097@gmail.com>
2025-05-19 11:26:23 +0700

    add 'if not exist'

--- src/main/resources/db/migration/202505080003__add_status_category_to_bookmarks_ddl.sql
+++ src/main/resources/db/migration/202505080003__add_status_category_to_bookmarks_ddl.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS categories
     CONSTRAINT pk_categories PRIMARY KEY (id)
 );
 
-ALTER TABLE bookmarks
-    ADD category_id BIGINT NULL;
-
-ALTER TABLE bookmarks
-    ADD status VARCHAR(255) NULL;
-
-ALTER TABLE bookmarks
-    ADD CONSTRAINT FK_BOOKMARKS_ON_CATEGORY FOREIGN KEY (category_id) REFERENCES categories (id);
+# ALTER TABLE bookmarks
+#     ADD category_id BIGINT NULL;
+#
+# ALTER TABLE bookmarks
+#     ADD status VARCHAR(255) NULL;
+#
+# ALTER TABLE bookmarks
+#     ADD CONSTRAINT FK_BOOKMARKS_ON_CATEGORY FOREIGN KEY (category_id) REFERENCES categories (id);
